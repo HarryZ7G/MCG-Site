718f225d661311582f87ba02be072b7f1c85089c
HarryZ7G <56236602+HarryZ7G@users.noreply.github.com>
2021-08-16 15:41:11 -0400

    Changed footer links to icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,54 +1,49 @@
 import React from "react";
 import "./Footer.css";
 
+// Media
+import Email from "../../media/Email.svg";
+import FaceBook from "../../media/FaceBook.svg";
+import Instagram from "../../media/Instagram.svg";
+import LinkedIn from "../../media/Linkedin.svg";
+
 function Footer(props) {
   const [title, setTitle] = React.useState("Connect with us");
+  const [size, setSize] = React.useState(null);
 
   const CopyEmail = () => {
-    setTitle("Copied to clipboard");
+    setTitle("rohan.chadha@mail.utoronto.ca");
+    setSize("small");
     setTimeout(() => {
       setTitle("Connect with us");
-    }, 4000);
-    navigator.clipboard.writeText(
-      "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-    );
+      setSize(null);
+    }, 10000);
   };
 
   return (
     <div className="footer">
-      <h2 className="white-font">{title}</h2>
+      <h2 className={`white-font ${size}`}>{title}</h2>
       <div className="contact-block">
-        <a>
-          <h3 className="white-font" onClick={CopyEmail}>
-            Email
-          </h3>
-        </a>
-        <a>
-          <h3
-            className="white-font"
-            onClick={() => window.open("https://www.facebook.com/UTSCMCG/")}
-          >
-            Facebook
-          </h3>
-        </a>
-        <a>
-          <h3
-            className="white-font"
-            onClick={() => window.open("https://www.instagram.com/mcg_utsc/")}
-          >
-            Instagram
-          </h3>
-        </a>
-        <a>
-          <h3
-            className="white-font"
-            onClick={() =>
-              window.open("https://www.linkedin.com/company/utscmcg/")
-            }
-          >
-            LinkedIn
-          </h3>
-        </a>
+        <button onClick={CopyEmail}>
+          <img src={Email} alt="Email" />
+        </button>
+        <button
+          onClick={() => window.open("https://www.facebook.com/UTSCMCG/")}
+        >
+          <img src={FaceBook} alt="FaceBook" />
+        </button>
+        <button
+          onClick={() => window.open("https://www.instagram.com/mcg_utsc/")}
+        >
+          <img src={Instagram} alt="Instagram" />
+        </button>
+        <button
+          onClick={() =>
+            window.open("https://www.linkedin.com/company/utscmcg/")
+          }
+        >
+          <img src={LinkedIn} alt="LinkedIn" />
+        </button>
       </div>
       <h3 className="white-font">(c) 2021 MCG - Management Consulting Group</h3>
     </div>
